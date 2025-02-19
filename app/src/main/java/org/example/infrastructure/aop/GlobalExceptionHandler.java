5b514b628270d23509a3c538622dec00e36447d1
Ekrem Beytula <78847462+Ekrem05@users.noreply.github.com>
2025-02-19 11:47:12 +0200

    exception handler added

---
+++ app/src/main/java/org/example/infrastructure/aop/GlobalExceptionHandler.java
@@ -0,0 +1,19 @@
+package org.example.infrastructure.aop;
+
+import com.auth0.jwt.exceptions.JWTDecodeException;
+import com.auth0.jwt.exceptions.JWTVerificationException;
+import com.fasterxml.jackson.core.JsonProcessingException;
+import org.springframework.http.HttpStatus;
+import org.springframework.http.ResponseEntity;
+import org.springframework.web.bind.annotation.ControllerAdvice;
+import org.springframework.web.bind.annotation.ExceptionHandler;
+
+@ControllerAdvice
+public class GlobalExceptionHandler {
+
+    @ExceptionHandler(Exception.class)
+    public ResponseEntity<String> handleException(Exception ex) {
+        return ResponseEntity.status(HttpStatus.INTERNAL_SERVER_ERROR)
+                .body("An error occurred: " + ex.getMessage());
+    }
+}
